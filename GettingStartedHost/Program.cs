853d87046988b040d0aa753d15789262a593e6c9
ckoda <ckoda@SWIZZSOFT>
2025-09-20 22:37:56 +0300

    Add project files.

---
+++ GettingStartedHost/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using GettingStartedLib;
+
+
+namespace GettingStartedHost
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // Step 1: Create a URI to serve as the base address.
+            Uri baseAddress = new Uri("http://localhost:8000/GettingStarted/");
+
+
+            //Step 2: Create a SericeHost instance.
+            ServiceHost selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
+
+
+            try 
+            {
+
+                //Step 3: Add a service endpoint
+                selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
+
+
+            }
+
+
+        }
+    }
+}
---
+++ GettingStartedLib/IService1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GettingStartedLib
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface ICalculator
+    {
+        [OperationContract]
+
+        double Add(double n1, double n2);
+
+        [OperationContract]
+        double Subtract(double n1, double n2);
+
+        [OperationContract]
+        double Multiply(double n1, double n2);
+
+        [OperationContract]
+        double Divide(double n1, double n2);
+
+
+        // TODO: Add your service operations here
+    }
+}
